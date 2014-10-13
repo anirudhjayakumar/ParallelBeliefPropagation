10090e6698c7c66ed49cdfde8cc98af9594a5714
Erin Carrier <ecarrie2@illinois.edu>
2014-10-13 18:57:03 -0500

    Updates to blur and downsample

--- testscript.py
+++ testscript.py
@@ -1,10 +1,21 @@
 import numpy as np
 import scipy as sp
 import scipy.io
+import scipy.signal
+import scipy.ndimage
 from PIL import Image
 import matplotlib.pyplot as plt
 
 mat = sp.io.loadmat("digits.mat")
 mat = mat['d'][:,0].reshape(28,28).T
-imgplt = plt.imshow(mat, interpolation='none')
+plt.imshow(mat, interpolation='none')
 plt.show()
+
+blurred = sp.ndimage.uniform_filter(mat)
+plt.imshow(blurred, interpolation='none')
+plt.show()
+
+small = blurred[::2, ::2]
+plt.imshow(small, interpolation='none')
+plt.show()
+
