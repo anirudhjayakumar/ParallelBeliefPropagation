c94811ba6f62f674b10442925e16741e75bfa676
Nate Bowman <nlbowma2@illinois.edu>
2014-10-13 20:07:27 -0500

    Adds ability to break images into subblocks.

--- testscript.py
+++ testscript.py
@@ -5,6 +5,21 @@ import scipy.ndimage
 import scipy.misc
 import matplotlib.pyplot as plt
 
+def blockshaped(arr, nrows, ncols):
+    """
+    Return an array of shape (n, nrows, ncols) where
+    n * nrows * ncols = arr.size
+
+    If arr is a 2D array, the returned array should look like n subblocks with
+    each subblock preserving the "physical" layout of arr.
+    Taken from Stackoverflow.com.
+    http://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
+    """
+    h, w = arr.shape
+    return (arr.reshape(h//nrows, nrows, -1, ncols)
+               .swapaxes(1,2)
+               .reshape(-1, nrows, ncols))
+
 #Read in image (mat file in this case)
 mat = sp.io.loadmat("digits.mat")
 mat = mat['d'][:,0].reshape(28,28).T
@@ -25,3 +40,7 @@ plt.show()
 newbig = sp.ndimage.zoom(small, 2, order=3)
 plt.imshow(newbig, interpolation='none')
 plt.show()
+
+#Break upsampled image into blocks
+PATCH_SIZE = 7
+blocks = blockshaped(small, PATCH_SIZE, PATCH_SIZE)
