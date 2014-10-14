721ff070de5e55a03a3e50b8cd1369f98827a65d
Erin Carrier <ecarrie2@illinois.edu>
2014-10-13 19:54:39 -0500

    Adds up sampled one.

--- testscript.py
+++ testscript.py
@@ -1,21 +1,27 @@
 import numpy as np
 import scipy as sp
 import scipy.io
-import scipy.signal
 import scipy.ndimage
-from PIL import Image
+import scipy.misc
 import matplotlib.pyplot as plt
 
+#Read in image (mat file in this case)
 mat = sp.io.loadmat("digits.mat")
 mat = mat['d'][:,0].reshape(28,28).T
 plt.imshow(mat, interpolation='none')
 plt.show()
 
+#Blur initial image
 blurred = sp.ndimage.uniform_filter(mat)
 plt.imshow(blurred, interpolation='none')
 plt.show()
 
+#Select only even indices to downsample image
 small = blurred[::2, ::2]
 plt.imshow(small, interpolation='none')
 plt.show()
 
+#Use cubic interpolation to upsample image
+newbig = sp.ndimage.zoom(small, 2, order=3)
+plt.imshow(newbig, interpolation='none')
+plt.show()
